c901c15e6b0eea17060794c42d99e05da206a0dd
Roman Khrebtov <khrebtov.dev@gmail.com>
2019-04-09 12:42:23 +0300

    Update pagination format

--- lib/gillbus/v2/structs/pagination.rb
+++ lib/gillbus/v2/structs/pagination.rb
@@ -3,10 +3,14 @@ module Gillbus::V2
     field :uuid, :string, from: "page_uuid"
     field :pages_count, :integer, from: "page_count"
     field :current_page, :integer, from: "page_number"
-    field :expires_in, :integer
+    field :expires_on, :date_time_rfc3339, from: "expires_in"
 
     def next_page
       current_page + 1 if current_page < pages_count
+    end
+
+    def expired?
+      !expires_on.nil? && expires_on <= DateTime.now.new_offset(0)
     end
   end
 end
